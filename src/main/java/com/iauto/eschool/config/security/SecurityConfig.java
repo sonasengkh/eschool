c59ff4c31b9d66698b6374ba9ce1029c4336a248
SENG Sona <sonaseng.kh@gmail.com>
2023-05-25 23:38:33 +0700

    9-jwt-v2

--- src/main/java/com/iauto/eschool/config/security/SecurityConfig.java
+++ src/main/java/com/iauto/eschool/config/security/SecurityConfig.java
@@ -3,15 +3,24 @@ package com.iauto.eschool.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.iauto.eschool.config.jwt.JwtLoginFilter;
+import com.iauto.eschool.config.jwt.TokenVerifyFilter;
+
 @Configuration
+@EnableGlobalMethodSecurity(
+		  prePostEnabled = true, 
+		  securedEnabled = true, 
+		  jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
@@ -20,14 +29,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
+			.addFilter(new JwtLoginFilter(authenticationManager()))
+			.addFilterAfter(new TokenVerifyFilter(), JwtLoginFilter.class)
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
 			.authorizeHttpRequests()
 			.antMatchers("/index.html").permitAll()
 			.antMatchers("*").hasRole("ADMIN")
 			.antMatchers("/courses*","/courses/*").hasAnyRole("ADMIN","AUTHOR")
 			.anyRequest()
 			.authenticated()
-			.and()
-			.httpBasic();
+			//.and()
+			//.httpBasic()
+			;
 	}
 	
 	@Bean
